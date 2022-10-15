94b9527184eaf66225868a89e946cd5540fad35f
berkcangumusisik <gumusisikberkcan@gmail.com>
2022-10-15 17:20:11 +0300

    3. Gün 1. Ödev Yüklendi.

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Merhaba Java"); // Ekrana Merhaba Java yazdırır.
-        System.out.println("Merhaba Java 2"); // Ekrana Merhaba Java 2 yazdırır.
-    }
-}
